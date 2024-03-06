ba037f3965c2443a6f1f8fb7201228bb694b9512
JW <jiangweixian1994@gmail.com>
2024-03-06 15:50:38 +0800

    feat: client ref manifest plugin

--- crates/node_binding/binding.d.ts
+++ crates/node_binding/binding.d.ts
@@ -177,7 +177,8 @@ export enum BuiltinPluginName {
   CssExtractRspackPlugin = 'CssExtractRspackPlugin',
   JsLoaderRspackPlugin = 'JsLoaderRspackPlugin',
   LazyCompilationPlugin = 'LazyCompilationPlugin',
-  RSCClientEntryRspackPlugin = 'RSCClientEntryRspackPlugin'
+  RSCClientEntryRspackPlugin = 'RSCClientEntryRspackPlugin',
+  RSCClientReferenceManifestRspackPlugin = 'RSCClientReferenceManifestRspackPlugin'
 }
 
 export function cleanupGlobalTrace(): void
