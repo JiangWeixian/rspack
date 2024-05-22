8f6687e052d737fb56b33567d1b0430a0ea1d784
JW <jiangweixian1994@gmail.com>
2024-05-22 17:18:08 +0800

    feat: rsc proxy and manifest plugin

--- packages/rspack/src/exports.ts
+++ packages/rspack/src/exports.ts
@@ -247,9 +247,9 @@ export { SourceMapDevToolPlugin } from "./builtin-plugin";
 export { EvalSourceMapDevToolPlugin } from "./builtin-plugin";
 export { EvalDevToolModulePlugin } from "./builtin-plugin";
 export { CssExtractRspackPlugin } from "./builtin-plugin";
-export { RSCClientEntryPlugin } from "./builtin-plugin";
+export { RSCClientEntryRspackPlugin } from "./builtin-plugin";
+export { RSCProxyRspackPlugin } from "./builtin-plugin";
 export { RSCClientReferenceManifestRspackPlugin } from "./builtin-plugin";
-export { RSCServerReferenceManifestRspackPlugin } from "./builtin-plugin";
 
 ///// Rspack Postfixed Internal Loaders /////
 export type {
