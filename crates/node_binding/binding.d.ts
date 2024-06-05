44a2a9a6286feb3f192569b7a82dfab49777a139
JW <jiangweixian1994@gmail.com>
2024-06-05 11:21:42 +0800

    fix: hmr

--- crates/node_binding/binding.d.ts
+++ crates/node_binding/binding.d.ts
@@ -3,8 +3,8 @@
 
 /* -- banner.d.ts -- */
 export type JsFilename =
-  | string
-  | ((pathData: JsPathData, assetInfo?: JsAssetInfo) => string);
+	| string
+	| ((pathData: JsPathData, assetInfo?: JsAssetInfo) => string);
 
 export type LocalJsFilename = JsFilename;
 
@@ -271,8 +271,7 @@ export enum BuiltinPluginName {
   JsLoaderRspackPlugin = 'JsLoaderRspackPlugin',
   LazyCompilationPlugin = 'LazyCompilationPlugin',
   RSCClientEntryRspackPlugin = 'RSCClientEntryRspackPlugin',
-  RSCClientReferenceManifestRspackPlugin = 'RSCClientReferenceManifestRspackPlugin',
-  RSCServerReferenceManifestRspackPlugin = 'RSCServerReferenceManifestRspackPlugin'
+  RSCClientReferenceManifestRspackPlugin = 'RSCClientReferenceManifestRspackPlugin'
 }
 
 export function cleanupGlobalTrace(): void
@@ -1438,6 +1437,11 @@ export interface RawResolveTsconfigOptions {
 
 export interface RawRscClientEntryRspackPluginOptions {
   routes?: Array<RawReactRoute>
+}
+
+export interface RawRscClientReferenceManifestRspackPluginOptions {
+  routes?: Array<RawReactRoute>
+  dev: boolean
 }
 
 export interface RawRspackFuture {
